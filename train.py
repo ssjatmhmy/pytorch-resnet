dc9aa22cdf11285b399a647a950c2df73cc0da2c
Xianfan Lin <ssjatmhmy@users.noreply.github.com>
2018-03-27 20:49:01 +0800

    Update train.py

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ Training script for ImageNet
 '''
 import argparse
 import os
+import json
 import shutil
 import time
 import random
@@ -17,18 +18,20 @@ import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import torchvision.models as models
 
+from resnet import resnet18
+
 # Parse arguments
 parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
 
 # Model options
 parser.add_argument('--depth', default=18, type=int)
 parser.add_argument('--widen', default=1, type=float, help='widen factor')
-parser.add_argument('--dataroot', default='path to dataset', type=str)
+parser.add_argument('--data', default='path to dataset', type=str)
 parser.add_argument('--nthread', default=4, type=int,
                     help='number of data loading threads (default: 4)')
 
 # Optimization options
-parser.add_argument('--epochs', default=90, type=int, metavar='N',
+parser.add_argument('--epochs', default=100, type=int, metavar='N',
                     help='number of total epochs to run')
 parser.add_argument('--train_batch', default=256, type=int, metavar='N',
                     help='train batchsize (default: 256)')
@@ -37,7 +40,7 @@ parser.add_argument('--test_batch', default=200, type=int, metavar='N',
 parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
                     metavar='LR', help='initial learning rate')
 parser.add_argument('--schedule', default='[30, 60, 90]', type=str, 
-                        help='Decrease learning rate at these epochs.')
+                        help='json list with epochs to drop lr on')
 parser.add_argument('--lr_decay_ratio', type=float, default=0.1, help='LR is multiplied by this ratio on schedule.')
 parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
                     help='momentum')
@@ -59,3 +62,71 @@ parser.add_argument('--gpu_id', default='0', type=str,
 
 args = parser.parse_args()
 state = {k: v for k, v in args._get_kwargs()}
+
+# Use CUDA
+os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu_id
+use_cuda = torch.cuda.is_available()
+
+def create_iterators():
+    """Data loading code"""
+    traindir = os.path.join(args.data, 'train')
+    valdir = os.path.join(args.data, 'val')
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+
+    train_loader = torch.utils.data.DataLoader(
+        datasets.ImageFolder(traindir, transforms.Compose([
+            transforms.RandomSizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize,
+        ])),
+        batch_size=args.train_batch, shuffle=True,
+        num_workers=args.nthread, pin_memory=torch.cuda.is_available())
+
+    val_loader = torch.utils.data.DataLoader(
+        datasets.ImageFolder(valdir, transforms.Compose([
+            transforms.Scale(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            normalize,
+        ])),
+        batch_size=args.test_batch, shuffle=False,
+        num_workers=args.nthread, pin_memory=torch.cuda.is_available())
+    
+    return train_loader, val_loader  
+  
+best_acc = 0  # best test accuracy
+
+def main():
+    global best_acc
+    
+    schedule = json.loads(args.schedule)
+    
+    if not os.path.isdir(args.checkpoint):
+        mkdir_p(args.checkpoint)
+    
+    # load dataset
+    train_loader, val_loader = create_iterators()
+    
+    # create model
+    model = resnet18()
+    model = torch.nn.DataParallel(model).cuda()
+    print('    Total params: %.2fM' % (sum(p.numel() for p in model.parameters())/1000000.0))
+    
+    # define loss function (criterion) and optimizer
+    criterion = nn.CrossEntropyLoss().cuda()
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+
+    # Train and val
+    start_epoch = 0
+    for epoch in range(start_epoch, args.epochs):
+        adjust_learning_rate(optimizer, epoch)
+
+        print('\nEpoch: [%d | %d] LR: %f' % (epoch + 1, args.epochs, state['lr']))
+
+        train_loss, train_acc = train(train_loader, model, criterion, optimizer, epoch, use_cuda)
+        test_loss, test_acc = test(val_loader, model, criterion, epoch, use_cuda)
+
+    
+
