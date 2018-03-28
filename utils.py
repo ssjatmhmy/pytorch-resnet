722d782d0e60daf806cb8cc466c19e6b9e1c4ec5
Xianfan Lin <ssjatmhmy@users.noreply.github.com>
2018-03-28 10:17:43 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -20,7 +20,22 @@ def mkdir_p(path):
             pass
         else:
             raise
+            
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
 
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
+            
 class AverageMeter(object):
     """Computes and stores the average and current value
        Imported from https://github.com/pytorch/examples/blob/master/imagenet/main.py#L247-L262
